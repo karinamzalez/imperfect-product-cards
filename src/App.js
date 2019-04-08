0995e36f3e238c2e826e0163347ea73a7e03f1cf
Jesse Bibee <jesse@imperfectproduce.com>
2019-04-08 14:00:57 -0700

    Add product data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Products from './Products';
+import products from './products.json';
 
 class App extends Component {
   render() {
@@ -20,6 +22,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Products products={products} />
       </div>
     );
   }
---
+++ src/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Products = ({ products }) => {
+  console.log({ products });
+
+  return (
+    <div>
+      Product cards here!!
+    </div>
+  );
+};
+
+export default Products;
