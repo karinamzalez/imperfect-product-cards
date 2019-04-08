24d69cff1452cbb093dce4c126486ed87282beda
Jesse Bibee <jesse@imperfectproduce.com>
2019-04-08 14:20:35 -0700

    Cloudinary image URL

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// cloudinary URLs:
+// res.cloudinary.com/imperfect/image/upload/w_400,h_260,c_pad,b_auto,d_products:no-image-found.png/{product.imageFilename}
+
 const Products = ({ product }) => {
   return (
     <div>
