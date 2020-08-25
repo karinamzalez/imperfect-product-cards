43b6e265ef4552bb4e30fe7f9775045c34c82a54
Karina Munoz Gonzalez <karinamzalez@gmail.com>
2020-08-24 20:47:41 -0500

    - update Filter.js: add component styling - delete Filters.css: remove dead code

--- src/Filters.js
+++ src/Filters.js
@@ -1,12 +1,23 @@
+// IMPORTS -----------------------------------
 import React from 'react';
-import './Filters.css';
+import styled from 'styled-components';
 
+// COMPONENT -----------------------------------
 const Filters = () => (
-  <div className="Filters-row">
+  <Row>
     <select onChange={(event) => console.log(event.target.value)}>
       <option value="All Products">All Products</option>
     </select>
-  </div>
+  </Row>
 );
 
+// STYLING -----------------------------------
+const Row = styled.div `
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+
+// EXPORTS -----------------------------------
 export default Filters;
