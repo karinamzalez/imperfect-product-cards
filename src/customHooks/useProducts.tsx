8fca479ab918ad9136892d2ff89b7305b81cd707
Karina Munoz Gonzalez <karinamzalez@gmail.com>
2020-08-25 15:47:51 -0500

    useProducts.tsx: add product data parser so each product has only one category

--- src/customHooks/useProducts.tsx
+++ src/customHooks/useProducts.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
-import { Product } from '../types';
+import { ProductType } from '../types';
 
-export default (initialValue: Product[]) => {
-  const [ products, setProducts ] = useState<Product[]>(initialValue);
+export default (initialValue: ProductType[]) => {
+  const [ products, setProducts ] = useState<ProductType[]>(initialValue);
+
+  // REMOVE DUPLICATE, HIGHER LEVEL CATEGORY (NON-PRODUCE/PRODUCE)
+  const parsedProducts = (newProducts: ProductType[]) => { 
+    return newProducts.map((product) => {
+      product.categories = product.categories.filter(category => !['PRO', 'DRY'].includes(category.commonId));
+      return product;
+    });
+  };
 
   return {
     products,
-    addProducts: (newProducts: Product[]) => {
+    addProducts: (newProducts: ProductType[]) => {
       // ADD NEW PRODUCTS TO CURRENTLY EXISTING LIST
-      setProducts([...products, ...newProducts]);
+      setProducts([...products, ...parsedProducts(newProducts)]);
     }
   };
 };
