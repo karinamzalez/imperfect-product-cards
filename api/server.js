6509ae3ed861a43128ee6e145c76ed3586fa5a0e
Karina Munoz Gonzalez <karinamzalez@gmail.com>
2020-08-25 11:20:36 -0500

    api/server.js: enable all CORS requests

--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const products = require('../src/products.json');
 
 const app = express();
+// ENABLE ALL CORS REQUESTS
+const cors = require("cors");
+app.use(cors());
 const port = process.env.PORT || 9001;
 
 app.get('/products', (req, res) => {
